fa7670cfd580c257b75dbbcb043b556fc5e22a06
Paulo Adrian Tijero Texeira <paulotijero@gmail.com>
2019-05-01 17:32:50 -0500

    Hotel validations (#58)
    * trash
    
    * Add validations hotel

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,4 +1,9 @@
 class Hotel < ApplicationRecord
+  validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/}
+  validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+  validates :city, presence: true, format: { with: /\A[a-zA-Z]+\z/ }
+  validates :country, presence: true, format: { with: /\A[a-zA-Z]+\z/ }
+  validates :address, presence: true
   has_many :rooms, dependent: :destroy
   has_many :promotions, as: :promotionable , dependent: :destroy
   has_many :bookings, through: :rooms
