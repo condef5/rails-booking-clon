c0fa99c3f37c30ab84f2a5b500f8ba13527d4851
Frank Davis Condezo Fabian <davis.con.fab@gmail.com>
2019-05-01 09:59:45 -0500

    Rooms images (#54)
    * Fixed close tag
    
    * Add updated and edit hotel
    
    * Add active storage
    
    * Add images to hotel
    
    * Remove unnecessary files
    
    * Add images to rooms

--- app/controllers/admin/rooms_controller.rb
+++ app/controllers/admin/rooms_controller.rb
@@ -1,18 +1,9 @@
 class Admin::RoomsController < ApplicationController
+  before_action :set_room, only: [:show, :edit, :update, :destroy]
 
   def new
     @hotel = Hotel.find(params[:hotel_id])
     @room = Room.new
-  end
-
-  def create
-    @hotel = Hotel.find(params[:hotel_id])
-    @room = @hotel.rooms.new(hotel_params)
-    if @room.save
-      redirect_to admin_hotel_path(@hotel), notice: 'Room was successfully created.'
-    else
-      render :new
-    end
   end
 
   def show
@@ -20,9 +11,35 @@ class Admin::RoomsController < ApplicationController
     @room = Room.find(params[:id])
   end
 
+  def edit
+  end
+
+  def create
+    @hotel = Hotel.find(params[:hotel_id])
+    @room = @hotel.rooms.new(room_params)
+    if @room.save
+      redirect_to admin_hotel_room_path(@hotel, @room), notice: 'Room was successfully created.'
+    else
+      render :new
+    end
+  end
+  
+  def update
+    if @room.update(room_params)
+      redirect_to admin_hotel_room_path(@hotel, @room), notice: 'Room was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   private
-  def hotel_params
-    params.require(:room).permit(:name ,:amount_of_beds ,:price)
+  def set_room
+    @hotel = Hotel.find(params[:hotel_id])
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:name, :amount_of_beds, :price, :image, gallery: [])
   end
 
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,8 @@ class Room < ApplicationRecord
   belongs_to :hotel
   has_many :promotions, as: :promotionable  , dependent: :destroy
   has_many :bookings
+  has_one_attached :image
+  has_many_attached :gallery
 
   validates :name, presence: true
   validates :price, numericality: { greater_than: 0 }
