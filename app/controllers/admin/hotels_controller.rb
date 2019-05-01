a22fded7b4183fca2f72ffef47372f0fa539906c
Frank Davis Condezo Fabian <davis.con.fab@gmail.com>
2019-05-01 09:58:24 -0500

    Edit hotel (#52)
    * Fixed close tag
    
    * Add updated and edit hotel

--- app/controllers/admin/hotels_controller.rb
+++ app/controllers/admin/hotels_controller.rb
@@ -1,40 +1,49 @@
 class Admin::HotelsController < ApplicationController
-  before_action :set_hotel, only: [:show, :destroy]
+  before_action :set_hotel, only: [:show, :edit, :update, :destroy]
   
-    def index
-      @hotels = Hotel.all
-    end
+  def index
+    @hotels = Hotel.all
+  end
 
-    def show
+  def edit
+  end
 
-    end
-  
-    def new
-        @hotel = Hotel.new
-    end
+  def show
+  end
 
-    def create
-        @hotel = Hotel.new(hotel_params)
-        if @hotel.save
-          redirect_to admin_hotel_path(@hotel), notice: 'hotel was successfully created.'
-        else
-          render :new
-        end
-    end
+  def new
+    @hotel = Hotel.new
+  end
 
-    def destroy
-      @hotel.destroy
-      redirect_to admin_hotels_path, notice: 'Hotel was successfully destroyed.'
+  def create
+    @hotel = Hotel.new(hotel_params)
+    if @hotel.save
+      redirect_to admin_hotel_path(@hotel), notice: 'hotel was successfully created.'
+    else
+      render :new
     end
+  end
 
-    private
-
-    def set_hotel
-      @hotel = Hotel.find(params[:id])
+  def update
+    if @hotel.update(hotel_params)
+      redirect_to admin_hotel_path(@hotel), notice: 'hotel was successfully updated.'
+    else
+      render :edit
     end
+  end
 
-    def hotel_params
-      params.require(:hotel).permit(:name ,:email ,:city ,:country ,:address)
-    end
+  def destroy
+    @hotel.destroy
+    redirect_to admin_hotels_path, notice: 'Hotel was successfully destroyed.'
+  end
+
+  private
+  def set_hotel
+    @hotel = Hotel.find(params[:id])
+  end
+
+  def hotel_params
+    params.require(:hotel).permit(:name ,:email ,:city ,:country ,:address)
+  end
 
 end
