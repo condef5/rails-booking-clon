d726322ef7a5a8da703e374256f71321d59b4227
Paulo Adrian Tijero Texeira <paulotijero@gmail.com>
2019-05-03 16:56:58 -0500

    Hotel promotion (#93)
    * trash
    
    * Fix add promotions hotel

--- app/controllers/admin/promotions_controller.rb
+++ app/controllers/admin/promotions_controller.rb
@@ -1,32 +1,28 @@
 class Admin::PromotionsController < ApplicationController
+  before_action :set_promotion, only: [:index, :new, :show, :edit, :create, :update]
 
   def index
-    @hotel = Hotel.find(params[:hotel_id])
     @promotions = @hotel.promotions
     authorize [:admin,  @promotions]
   end
 
   def new
-    @hotel = Hotel.find(params[:hotel_id])
     @promotion = Promotion.new
     authorize [:admin,  @promotion]
   end
 
   def show
-    @hotel = Hotel.find(params[:hotel_id])
     @promotion = Promotion.find(params[:id])
     authorize [:admin,  @promotion]
   end
 
   def edit
-    @hotel = Hotel.find(params[:hotel_id])
     @promotion = Promotion.find(params[:id])
     authorize [:admin,  @promotion]
   end
 
   def create
-    @hotel = Hotel.find(params[:hotel_id])
-    @promotion = @hotel.promotions.new(hotel_params)
+    @promotion = @hotel.promotions.new(promotions_params)
     authorize [:admin,  @promotion]
     if @promotion.save
       redirect_to admin_hotel_path(@hotel), notice: 'Promotion was successfully created.'
@@ -36,10 +32,9 @@ class Admin::PromotionsController < ApplicationController
   end
 
   def update 
-    @hotel = Hotel.find(params[:hotel_id])
     @promotion = Promotion.find(params[:id])
     authorize [:admin,  @promotion]
-    if @promotion.update(promotion_params)
+    if @promotion.update(promotions_params)
       redirect_to admin_hotel_promotions_path(@hotel), notice: 'Promotion was successfully updated'
     else 
       render :new
@@ -47,7 +42,11 @@ class Admin::PromotionsController < ApplicationController
   end
 
   private
-  def promotion_params
+  def set_promotion
+    @hotel = Hotel.find(params[:hotel_id])
+  end
+
+  def promotions_params
     params.require(:promotion).permit(:start_date, :end_date, :discount_type, :discount_amount)
   end
 end
