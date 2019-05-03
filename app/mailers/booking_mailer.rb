ebee6e628b10f857c0590f5082a62921fd5f6ae5
MariethPerez <mperezzevallos@gmail.com>
2019-05-03 11:29:59 -0500

    Add booking created mailer (#79)
    * Add booking created mailer
    
    * Fix model booking
    
    * Add deliver_later in method for email

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -6,4 +6,14 @@ class BookingMailer < ApplicationMailer
     @booking = params[:booking]
     mail(to: @user.email, subject: 'Booking deleted')
   end
+
+  def booking_created_user
+    @booking = params[:booking]
+    mail(to: @booking.user.email, subject: 'Booking created')
+  end
+
+  def booking_created_hotel
+    @booking = params[:booking]
+    mail(to: @booking.room.hotel.email, subject: 'Booking created')
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,15 @@
 class Booking < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  after_create :notification_booking_created
 
   def self.available?(min_date, max_date)
     where('end_date >= ? AND start_date <= ?', min_date, max_date)
   end
+
+  def notification_booking_created
+    BookingMailer.with(booking: self).booking_created_user.deliver_later
+    BookingMailer.with(booking: self).booking_created_hotel.deliver_later
+  end
+
 end
