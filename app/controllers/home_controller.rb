c8ea75646cf0a30d8cb460001416505ef963cd6c
Frank Davis Condezo Fabian <davis.con.fab@gmail.com>
2019-05-02 21:20:28 -0500

    Feature/search rooms (#72)
    * Add partial search
    
    * Add partial search in views index and search
    
    * Add method and route search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,18 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def index
     if current_user && current_user.has_role?('admin')
       redirect_to admin_root_path
     end
+  end
+  
+  def search
+    # it is necessary to validate the rented roms and not show them in the list
+    query = {}
+    query[:price] = params[:price_min]..params[:price_max] if params[:price_max].present? && params[:price_min].present? 
+    query[:amount_of_beds] = params[:num_beds] if params[:num_beds].present?
+    @rooms = Room.joins(:hotel).where(query)
+    @rooms = @rooms.where("hotels.city ILIKE ?", "%#{params[:destiny]}%")  if params[:destiny].present?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: "home#index"
-
+  get '/search', to: 'home#search'
   
   namespace :admin do
     root to: "hotels#index"
