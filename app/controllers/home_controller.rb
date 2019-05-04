ff6ac48de5ed7a000483608e9c73cee43e2a5032
Frank Davis Condezo Fabian <davis.con.fab@gmail.com>
2019-05-03 23:26:51 -0500

    Feature/filter dates (#123)
    * Refactor views
    
    * Filter range dates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,12 +9,18 @@ class HomeController < ApplicationController
   end
   
   def search
-    # TODO: it is necessary to validate the rented roms and not show them in the list
     query = {}
     query[:price] = params[:price_min]..params[:price_max] if params[:price_max].present? && params[:price_min].present? 
     query[:amount_of_beds] = params[:num_beds] if params[:num_beds].present?
     @rooms = Room.joins(:hotel).where(query)
     @rooms = @rooms.where("hotels.city ILIKE ?", "%#{params[:destiny]}%")  if params[:destiny].present?
+    # Validate dates taking
+    if params[:start_date].present? && params[:end_date].present? 
+      @rooms = @rooms.select do |room|
+        room.bookings.available?(params[:start_date], params[:end_date]).empty?
+      end
+    end
+
   end
 
   def promotions
